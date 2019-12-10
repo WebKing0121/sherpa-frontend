124c507799ed234c9496fae7c35ed1b7038e39e3
webking0121 <bestwebdev010@gmail.com>
2019-12-09 17:51:51 -0800

    Updates campaign and market API calls to run synchronously

--- src/store/CampaignFolders/actions.js
+++ src/store/CampaignFolders/actions.js
@@ -1,47 +1,50 @@
-import axios from 'axios';
-import AxiosInstance from '../../axiosConfig';
-import { SET_FETCH_CAMPAIGN_FOLDERS, SET_FETCH_CAMPAIGN_FOLDERS_ERROR } from './actionTypes';
-import { createFolders, chkForMultipleMarkets } from './transformers';
-import { history } from '../../history';
+import axios from "axios";
+import AxiosInstance from "../../axiosConfig";
+import { SET_FETCH_CAMPAIGN_FOLDERS, SET_FETCH_CAMPAIGN_FOLDERS_ERROR } from "./actionTypes";
+import { createFolders, chkForMultipleMarkets } from "./transformers";
+import { history } from "../../history";
 
-export const setFetchedCampaignFolders = (campaignFolders) => ({
+export const setFetchedCampaignFolders = campaignFolders => ({
   type: SET_FETCH_CAMPAIGN_FOLDERS,
   campaignFolders
 });
 
-export const setFetchedCampaignFoldersError = (error) => ({
+export const setFetchedCampaignFoldersError = error => ({
   type: SET_FETCH_CAMPAIGN_FOLDERS_ERROR,
   error
 });
 
 export const fetchCampaignFolders = () => (dispatch, _) => {
   // NOTE: Needs to hit the Folder-endpoint in the future
-  // For now we will render 1 folder called ALL that will contain
-  // all campaigns
-  const fetchCampaigns = AxiosInstance.get('/campaigns/');
-  const fetchMarkets = AxiosInstance.get('/markets/');
+  // For now we will render 1 folder called ALL that will contain all campaigns
+  const handleError = (error, message) => {
+    console.log("error campaigns", error.response);
+    dispatch(setFetchedCampaignFoldersError(message));
+  };
 
-  axios.all([fetchCampaigns, fetchMarkets])
-    .then(axios.spread((campaigns, markets) => {
-      const { data: { results: campsData } } = campaigns;
-      const { data: { results: marksData } } = markets;
-
-      const marketIds = chkForMultipleMarkets(campsData);
-
-      if (marketIds.length > 1) {
-        dispatch(
-          setFetchedCampaignFolders(
-            createFolders(campsData, marksData)
-          )
-        )
-      } else {
-        let id = marketIds[0];
-        history.push(`/folder/${id}/campaigns`);
-      }
-
-    }))
-    .catch((error) => {
-      console.log('error campaigns', error.response);
-      dispatch(setFetchedCampaignFoldersError("Error when fetching campaigns"))
+  AxiosInstance.get("/campaigns/")
+    .then(campaigns => {
+      AxiosInstance.get("/markets/")
+        .then(markets => {
+          const {
+            data: { results: campsData }
+          } = campaigns;
+          const {
+            data: { results: marksData }
+          } = markets;
+          const marketIds = chkForMultipleMarkets(campsData);
+          if (marketIds.length > 1) {
+            dispatch(setFetchedCampaignFolders(createFolders(campsData, marksData)));
+          } else {
+            let id = marketIds[0];
+            history.push(`/folder/${id}/campaigns`);
+          }
+        })
+        .catch(error => {
+          handleError(error, "Error when fetching markets");
+        });
+    })
+    .catch(error => {
+      handleError(error, "Error when fetching campaigns");
     });
-}
+};
