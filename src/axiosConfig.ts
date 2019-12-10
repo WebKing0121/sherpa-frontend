a04d8c169b9db29c4e8a891082e62837618dc9d5
webking0121 <bestwebdev010@gmail.com>
2019-12-09 16:12:49 -0800

    Adds refresh token functionality to fetch new access token when access token is expired and refresh token is still valid

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,13 +1,21 @@
-import axios from 'axios';
-import ReduxStore from './store/store';
-import { logout } from './store/Auth/actions';
-import { history } from './history';
-import { setAuthToken } from './store/Auth/utils';
-
+import axios from "axios";
+import ReduxStore from "./store/store";
+import { logout, setAuthenticated } from "./store/Auth/actions";
+import { setAuthToken, CreateToken, saveToLocalStorage, loadTokens } from "./store/Auth/utils";
 
 const axiosInstance = axios.create({
   baseURL: `${process.env.REACT_APP_BASE_URL}`
-})
+});
+
+// updates access token if is an error 401 AND refresh token is valid
+const handleNewAccessToken = (error: any, { access, refresh }: CreateToken) => {
+  const originalRequest = error.config;
+  originalRequest.headers["Authorization"] = `Bearer ${access}`;
+  saveToLocalStorage("access", access);
+  setAuthToken({ access });
+  ReduxStore.dispatch(setAuthenticated({ access, refresh }));
+  return originalRequest;
+};
 
 // adds interceptor for 401's
 const okResponseInterceptor = (response: any) => response;
@@ -15,23 +23,26 @@ const okResponseInterceptor = (response: any) => response;
 const errorResponseInterceptor = (error: any) => {
   // Redirect to login screen if we get `Unauthorized`
   if (error.response.status === 401) {
-    ReduxStore.dispatch(logout());
-    history.push("/login");
-
-    // ReduxStore.dispatch(refreshToken())
-    //   .then((token) => {
-    //     //tokens refreshed - go ahead and retry the API call again
-    //     setAuthToken({ access: token });
-    //     return axiosInstance.request(error.config)
-    //   });
+    // get new access token
+    const { auth } = loadTokens();
+    return axios
+      .post(`${process.env.REACT_APP_BASE_URL}auth/jwt/refresh/`, {
+        refresh: auth.refreshToken
+      })
+      .then(({ data }) => {
+        const updatedTokens = {
+          access: data.access,
+          refresh: auth.refreshToken
+        };
+        return axios.request(handleNewAccessToken(error, updatedTokens));
+      })
+      .catch(error => console.log("Token cannot be refreshed ", error));
   }
-  return Promise.reject(error)
-}
+  ReduxStore.dispatch(logout());
+  return Promise.reject(error);
+};
 
 // handle case where we get =401= unauthorized
-axiosInstance.interceptors.response.use(
-  okResponseInterceptor,
-  errorResponseInterceptor
-);
+axiosInstance.interceptors.response.use(okResponseInterceptor, errorResponseInterceptor);
 
 export default axiosInstance;
--- src/store/Auth/actions.ts
+++ src/store/Auth/actions.ts
@@ -1,63 +1,64 @@
-import AxiosInstance from '../../axiosConfig';
+import AxiosInstance from "../../axiosConfig";
 
-import {
-  setAuthToken, revokeAuthToken,
-  saveToLocalStorage, removeFromLocalStorage
-} from '../Auth/utils';
+import { setAuthToken, revokeAuthToken, saveToLocalStorage, removeFromLocalStorage } from "../Auth/utils";
 
-import { UserData } from './reducers';
+import { UserData } from "./reducers";
 
-export const setAuthenticated = ({ access = '', refresh = '' }) => ({
-  type: 'SET_AUTH_STATE',
+export const setAuthenticated = ({ access = "", refresh = "" }) => ({
+  type: "SET_AUTH_STATE",
   access,
   refresh
 });
 
-export const unsetAuthenticated = () => ({ type: 'SET_UNAUTH_STATE' });
-export const setAuthError = (error: string) => ({ type: 'SET_AUTH_ERROR', error })
-export const setUserInfo = (userData: UserData) => ({ type: 'SET_USER_DATA', userData })
+export const unsetAuthenticated = () => ({ type: "SET_UNAUTH_STATE" });
+export const setAuthError = (error: string) => ({ type: "SET_AUTH_ERROR", error });
+export const setUserInfo = (userData: UserData) => ({ type: "SET_USER_DATA", userData });
 
 export const fetchUserInfo = (dispatch: any) => {
-  return AxiosInstance.get('/auth/users/me/')
+  return AxiosInstance.get("/auth/users/me/")
     .then(({ data }) => {
-      dispatch(setUserInfo(data))
-      saveToLocalStorage('userData', JSON.stringify(data));
-      console.log("fetch-user-success response", data)
+      dispatch(setUserInfo(data));
+      saveToLocalStorage("userData", JSON.stringify(data));
+      console.log("fetch-user-success response", data);
     })
     .catch(({ response }) => {
-      console.log('fetch-user-error response', response);
-    })
-}
+      console.log("fetch-user-error response", response);
+    });
+};
+
+export const updateAuth = (dispatch: any, data: any) => {
+  dispatch(setAuthenticated(data));
+  saveToLocalStorage("access", data.access);
+  saveToLocalStorage("refresh", data.refresh);
+};
 
 export const authenticate = (credentials: any, continuation: any) => {
   return (dispatch: any, _: any) => {
-    return AxiosInstance
-      .post('auth/jwt/create/', credentials)
+    return AxiosInstance.post("auth/jwt/create/", credentials)
       .then(({ data }) => {
         setAuthToken(data);
 
         // fetch the user info, mainly the user-id
-        fetchUserInfo(dispatch)
-          .then(() => {
-            dispatch(setAuthenticated(data));
-            saveToLocalStorage('access', data.access);
-            saveToLocalStorage('refresh', data.refresh);
-            continuation();
-          })
+        fetchUserInfo(dispatch).then(() => {
+          updateAuth(dispatch, data);
+          continuation();
+        });
       })
       .catch(({ response }) => {
-        const { data: { detail } } = response;
+        const {
+          data: { detail }
+        } = response;
         dispatch(setAuthError(detail));
-      })
+      });
   };
-}
+};
 
 export const logout = (): any => {
   return (dispatch: any, _: any) => {
     dispatch(unsetAuthenticated());
-    revokeAuthToken('');
-    removeFromLocalStorage('access');
-    removeFromLocalStorage('refresh');
-    removeFromLocalStorage('userData');
-  }
-}
+    revokeAuthToken("");
+    removeFromLocalStorage("access");
+    removeFromLocalStorage("refresh");
+    removeFromLocalStorage("userData");
+  };
+};
