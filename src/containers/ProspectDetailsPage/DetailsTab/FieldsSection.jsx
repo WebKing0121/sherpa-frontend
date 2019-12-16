00d0732b1526eacd6f0e5552a38d4207a849bf57
webking0121 <bestwebdev010@gmail.com>
2019-12-16 14:27:16 -0800

    Update CRM options styling on Prospect Details [ch1089]

--- src/containers/ProspectDetailsPage/DetailsTab/FieldsSection.jsx
+++ src/containers/ProspectDetailsPage/DetailsTab/FieldsSection.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import InputGroupBorder from '../../../components/InputGroupBorder';
-import { Label, Input, InputGroupAddon, Button } from 'reactstrap';
+import { Label, Input, InputGroupAddon, Button, FormGroup, CustomInput } from 'reactstrap';
 import IconBg from '../../../components/IconBg';
 import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
@@ -11,6 +11,33 @@ import {
 } from '../../../store/ProspectDetails/selectors';
 import { fetchAgents } from '../../../store/ProspectDetails/actions';
 import InputSelect from '../../../components/InputSelect';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Modal from '../../../components/Modal';
+
+const BtnHolster = styled.div`
+  display: flex;
+  padding-top: var(--pad3);
+  justify-content: space-between;
+
+  button {
+    flex-basis: 50%;
+
+    &:first-child {
+      margin-right: var(--pad2);
+    }
+  }
+`;
+
+const Radio = styled(CustomInput)`
+  font-size: 1.25rem;
+  line-height: 1.2;
+  margin-bottom: .6em;
+  font-weight: bold;
+
+  &[disabled] {
+    color: var(--mediumGray);
+  }
+`;
 
 const FieldWrapper = styled.div`
   &:not(:last-child) {
@@ -56,6 +83,7 @@ const FieldsSection = props => {
   const campaigns = useSelector(prospectDetailsCampaigns);
   const agents = useSelector(agentSelector);
   const dispatch = useDispatch();
+  const [modal, setModal] = useState(false);
 
   useEffect(() => {
     // if there's a prospect we want to get the company-id to fetch agents
@@ -94,9 +122,29 @@ const FieldsSection = props => {
         icon='mobile-alt'
       />
 
-      <Field id='crm' label='CRM Options' placeholder='Select CRM Option' icon='share' />
-
       <Field id='reminder' label='Reminder' placeholder='Set a Reminder' icon='bell' />
+
+      <Label>CRM Options</Label>
+
+      <BtnHolster>
+        <Button id='zapier' color="primary" className="fw-bold" onClick={() => setModal(true)}>Email to CRM</Button>
+        <Button id='crm' color="primary" className="fw-bold" onClick={() => setModal(true)}>Push to Zapier</Button>
+        {/*  This should be the success state below:
+        <Button id='crm' color="success" className="fw-bold">
+          <FontAwesomeIcon icon="check" className="mr-1" />
+          Sent!
+        </Button> */}
+        <Modal isOpen={modal} toggle={() => setModal(false)} title='Campaigns'>
+          <Label><h4>Complete your action using the following campaign:</h4></Label>
+          <FormGroup className="mt-1 mb-3">
+            <Radio type="radio" name="campaigns" label="San Francisco" id="camp1"/>
+            <Radio type="radio" name="campaigns" label="Seattle/Tac" id="camp2"/>
+            <Radio type="radio" name="campaigns" label="Another Campaign" id="camp3"/>
+          </FormGroup>
+          <Button color="primary" block size="lg">Submit</Button>
+        </Modal>
+      </BtnHolster>
+
     </>
   );
 };
