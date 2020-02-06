e2fe2f004cdb3a5c6b44a5996f108b27596846e6
webking0121 <bestwebdev010@gmail.com>
2020-02-05 17:58:37 -0800

    quick reply comonents for desktop and mobile [ch1869]

--- src/components/messageTab/MessageInput.jsx
+++ src/components/messageTab/MessageInput.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Input, InputGroupAddon, Button } from 'reactstrap';
+import { Input, InputGroupAddon, Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 import InputGroupBorder from '../InputGroupBorder';
 import IconBg from '../IconBg';
 import Modal from '../Modal';
@@ -11,6 +11,21 @@ import { fetchQuickReplies } from '../../store/SmsTemplateStore/actions';
 import { quickRepliesPlaceholderText, Success, Fetching } from '../../helpers/variables';
 import { DataLoader } from '../LoadingData';
 import { useParams } from 'react-router-dom';
+import * as vars from '../../helpers/variables';
+
+const QuickReplies = styled.ul`
+  padding: 0;
+
+  li {
+    list-style: none;
+    font-weight: bold;
+    padding: var(--pad1) var(--pad3);
+
+    &:active {
+      background: var(--lightGray);
+    }
+  }
+`;
 
 const SendMessage = styled.form`
   padding: var(--pad2) var(--pad3);
@@ -18,10 +33,52 @@ const SendMessage = styled.form`
   background: white;
 `;
 
+const PopBody = styled(PopoverBody)`
+  padding: var(--pad1) var(--pad1) !important;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: space-between;
+
+  button {
+    color: var(--darkNavy);
+  }
+`;
+
+const QuickReplyDesktop = styled(Popover)`
+  --xpad: var(--pad3);
+  --ypad: var(--pad2);
+  --xypad: var(--ypad) var(--xpad);
+
+  .popover {
+    max-width: 400px;
+  }
+
+  .popover-header {
+    background-image: var(--tealBlueGradient);
+    color: var(--white);
+    display: flex;
+    justify-content: space-between;
+    padding: var(--xypad);
+  }
+  .popover-inner {
+    max-height: 400px;
+    overflow-y: scroll;
+  }
+  .popover-body {
+    padding: 0 !important;
+  }
+
+  .arrow {
+    left: 2rem !important;
+  }
+`;
+
 function MessageInput(props) {
   const { addNewMessage } = props;
   const [modal, setModal] = useState(false);
   const [input, setInput] = useState('');
+  const [isMobile, setIsMobile] = useState(window.innerWidth < vars.maxMobileWidth);
   const [isFetchingMessages, setIsFetchingMessages] = useState(false);
   // quick replies use the data loader component, which using a string instead of a boolean
   const [isFetchingQuickReplies, setIsFetchingQuickReplies] = useState(Success);
@@ -69,20 +126,42 @@ function MessageInput(props) {
       data={(quickReplies.length && quickReplies)}
       emptyResultsMessage={quickRepliesPlaceholderText}
       renderData={() => (
-        <ul style={style.ul} data-test='quick-replies'>{
+        <QuickReplies data-test='quick-replies'>{
           quickReplies.map(reply => (
             <li
-              style={style.li}
+              className="textM"
               key={reply.id}
               onClick={() => handleClickQuickReply(reply)}
             >
               {reply.question}
             </li>
           ))
-        }</ul>
+        }</QuickReplies>
       )}
     />;
   };
+
+  const quickReplyDesktop =(
+    <QuickReplyDesktop placement="bottom" isOpen={modal} toggle={toggle} target="quick-reply">
+      <PopoverHeader className="textXL">
+        Select Quick Reply
+        <FontAwesomeIcon icon="times" onClick={toggle}/>
+      </PopoverHeader>
+      <PopBody>
+        {mapQuickReplies()}
+      </PopBody>
+    </QuickReplyDesktop>
+  );
+
+  const quickReplyMobile =(
+    <Modal
+      isOpen={modal}
+      toggle={toggle}
+      title='Select Quick Reply'
+    >
+      {mapQuickReplies()}
+    </Modal>
+  );
 
   return (
     <SendMessage onSubmit={handleSubmit}>
@@ -93,6 +172,7 @@ function MessageInput(props) {
             className='p-0'
             type='button'
             color='link'
+            id="quick-reply"
             onClick={() => setModal(true)}
           >
             <FontAwesomeIcon icon='layer-group' color='gray' size='2x' className='mr-3' />
@@ -123,33 +203,10 @@ function MessageInput(props) {
           </Button>
         </InputGroupAddon>
       </InputGroupBorder>
-      <Modal
-        isOpen={modal}
-        toggle={toggle}
-        title='Select Quick Reply'
-      >
-        {mapQuickReplies()}
-      </Modal>
+      { isMobile ? quickReplyMobile : quickReplyDesktop }
 
     </SendMessage>
   );
 }
 
 export default MessageInput;
-
-
-const style = {
-  ul: {
-    padding: '0'
-  },
-  li: {
-    listStyle: 'none',
-    fontWeight: 'bold',
-    margin: '10px'
-  },
-  p: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: '20px'
-  }
-};
